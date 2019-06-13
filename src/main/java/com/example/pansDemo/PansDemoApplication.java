d46d694d4ef42160ee0e217ae33fbfbd973b3cee
pansai <1969755712@qq.com>
2019-06-13 16:47:41 +0800

    first

---
+++ src/main/java/com/example/pansDemo/PansDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.pansDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PansDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PansDemoApplication.class, args);
+	}
+
+}
